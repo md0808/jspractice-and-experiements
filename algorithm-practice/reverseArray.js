337e1d89e1333d1a063c541593cde1ac08b3cea3
Monica <monica.dixon8@gmail.com>
2020-05-05 18:52:06 -0500

    reverse in place array

---
+++ algorithm-practice/reverseArray.js
@@ -0,0 +1,10 @@
+const reverseArrayInPlace = (arr) => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let tempVar = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempVar;
+  }
+  return arr;
+};
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));

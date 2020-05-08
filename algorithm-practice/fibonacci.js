9a3a2257effd075b264acd2d29b7b0b5c1c7f1d7
Monica <monica.dixon8@gmail.com>
2020-05-08 12:19:25 -0500

    memoization for fibonacci

--- algorithm-practice/fibonacci.js
+++ algorithm-practice/fibonacci.js
@@ -6,3 +6,34 @@ const fibonacci = (position) => {
 
 console.log(fibonacci(9));
 // trying to find the number that is at the the position in the array (+1, because not index.)
+
+const memoizedFib = (index, cache) => {
+  cache = cache || [];
+  if (cache[index]) {
+    return cache[index];
+  } else {
+    if (index < 3) {
+      return 1;
+    } else {
+      cache[index] =
+        memoizedFib(index - 1, cache) + memoizedFib(index - 2, cache);
+    }
+  }
+  return cache[index];
+};
+
+console.log(memoizedFib(20));
+
+function fibMemo(index, cache) {
+  cache = cache || [];
+  if (cache[index]) return cache[index];
+  else {
+    if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    }
+  }
+  return cache[index];
+}
+
+console.log(fibMemo(20));
